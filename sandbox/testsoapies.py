4ffcca0cf51e57e4819b8f5aac3ea71959cb470c
Matthew Wiesner <wiesner@jhu.edu>
2022-07-10 11:45:59 -0400

    fixes soapies test

--- sandbox/testsoapies.py
+++ sandbox/testsoapies.py
@@ -1,6 +1,6 @@
 from lhotse.recipes import download_soapies, prepare_soapies
 
-download_soapies("sandbox/root/soapies", languages="xhosa")
-manifest = prepare_soapies("sandbox/root/soapies", languages="xhosa")
+download_soapies("/expscratch/mwiesner/jsalt2022/data/soapies", languages="xhosa")
+manifest = prepare_soapies("/expscratch/mwiesner/jsalt2022/data/soapies", output_dir="/expscratch/mwiesner/jsalt2022/lhotse/soapies_manifests", languages="xhosa")
 
 import pdb; pdb.set_trace()
