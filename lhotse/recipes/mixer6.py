9dc5d7584bae4fa396447e8de58364e1fad03c25
Matthew Wiesner <wiesner@jhu.edu>
2023-01-12 16:48:02 -0500

    Small fix to mixer6 recipe to exclude any missing channels

--- lhotse/recipes/mixer6.py
+++ lhotse/recipes/mixer6.py
@@ -19,7 +19,7 @@ dataset_parts = [
 
 def prepare_mixer6(
     corpus_dir: Pathlike,
-    transcript_dir: Optional[Pathlike],
+    transcript_dir: Pathlike,
     output_dir: Optional[Pathlike] = None,
     part: str = "train_intv",
     channels: list = list(range(13)),
@@ -53,14 +53,14 @@ def prepare_mixer6(
         reco = Recording(
             id=session_id,
             sources = [
-                AudioSource(type="file", channels=[c], source=p) for c, p in paths
+                AudioSource(type="file", channels=[c], source=str(p)) for c, p in paths if p.is_file()
             ],
             sampling_rate=int(audio_sf.samplerate),
             num_samples=int(audio_sf.frames),
             duration=float(audio_sf.frames) / audio_sf.samplerate,
         )
         recordings.append(reco)
-      
+    
     # Prepare supervisions
     transcript_dir = transcript_dir / "splits" / part
     transcripts = transcript_dir.rglob("*.json") 
@@ -73,28 +73,34 @@ def prepare_mixer6(
             spkid, text = seg['speaker'], seg['words']
             start, end = float(seg['start_time']), float(seg['end_time'])
             begstr = format(int(format(start, '0.3f').replace('.', '')), '08d')
+            if end - start <= 0.01:
+                raise ValueError("The duration is too short or negative. This "
+                    "indicates a problem in the data")
             for chn in channels:
-                supervisions.append(
-                    SupervisionSegment(
-                        id=f"{spkid}-{session_id}-{chnid2name(chn)}-{begstr}",
-                        recording_id=session_id,
-                        start=start,
-                        duration=round(end - start, 4),
-                        channel=chn,
-                        language="English",
-                        speaker=spkid,
-                        text=text,
+                filepath = pcm_files / chnid2name(chn) / f"{session_id}_{chnid2name(chn)}.flac"
+                if filepath.is_file():
+                    supervisions.append(
+                        SupervisionSegment(
+                            id=f"{spkid}-{session_id}-{chnid2name(chn)}-{begstr}",
+                            recording_id=session_id,
+                            start=start,
+                            duration=round(end - start, 4),
+                            channel=chn,
+                            language="English",
+                            speaker=spkid,
+                            text=text,
+                        )
                     )
-            )
     
     recording_set = RecordingSet.from_recordings(recordings)
     supervision_set = SupervisionSet.from_segments(supervisions)
-    recording_set, supervisions_set = fix_manifests(recording_set, supervision_set)
+    recording_set, supervision_set = fix_manifests(recording_set, supervision_set)
     validate_recordings_and_supervisions(recording_set, supervision_set)
 
     if output_dir is not None:
-        recording_set.to_file(output_dir / f"recordings.jsonl")
-        supervision_set.to_file(output_dir / f"supervisions.jsonl")
+        output_dir = Path(output_dir)
+        recording_set.to_file(str(output_dir / f"recordings_{part}.jsonl.gz"))
+        supervision_set.to_file(str(output_dir / f"supervisions_{part}.jsonl.gz"))
 
     manifest = {"recordings": recording_set, "supervisions": supervision_set}
     return manifest
